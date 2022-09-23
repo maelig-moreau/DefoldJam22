40eebf76326fa191fcbe8137da05c778a2a68191
Maëlig Moreau <maelig.moreau@gmail.com>
2022-09-24 00:31:00 +0200

    timer
    and more scientists, and level design

--- DefoldJam22/main/player/player.go
+++ DefoldJam22/main/player/player.go
@@ -28,6 +28,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "timer"
+  component: "/menu/timer.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "rivemodel"
   type: "rivemodel"
